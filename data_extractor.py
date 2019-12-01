40cbfdb3fd6140d3696bf7abcb818d910748ec29
ieremiasviorel <ieremiasviorel@yahoo.com>
2019-12-01 19:23:31 +0200

    - crop images to bounding box containing the dog

---
+++ data_extractor.py
@@ -0,0 +1,79 @@
+import os
+import xml.etree.ElementTree as ET
+
+import cv2
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+
+breed_list = os.listdir("input/stanford-dogs-dataset/images/Images/")
+
+
+def label_assignment(img, label):
+    return label
+
+
+X = []
+Z = []
+imgsize = 150
+
+
+def training_data(label, data_dir):
+    for img in tqdm(os.listdir(data_dir)):
+        label = label_assignment(img, label)
+        path = os.path.join(data_dir, img)
+        img = cv2.imread(path, cv2.IMREAD_COLOR)
+        img = cv2.resize(img, (imgsize, imgsize))
+
+        X.append(np.array(img))
+        Z.append(str(label))
+
+
+num_classes = len(breed_list)
+print("{} breeds".format(num_classes))
+
+n_total_images = 0
+for breed in breed_list:
+    n_total_images += len(os.listdir("input/stanford-dogs-dataset/images/Images/{}".format(breed)))
+print("{} images".format(n_total_images))
+
+label_maps = {}
+label_maps_rev = {}
+for i, v in enumerate(breed_list):
+    label_maps.update({v: i})
+    label_maps_rev.update({i: v})
+
+
+def show_dir_images(breed, n_to_show):
+    plt.figure(figsize=(16, 16))
+    img_dir = "input/stanford-dogs-dataset/images/Images/{}/".format(breed)
+    images = os.listdir(img_dir)[:n_to_show]
+    for ii in range(n_to_show):
+        img = mpimg.imread(img_dir + images[ii])
+        plt.subplot(n_to_show / 4 + 1, 4, ii + 1)
+        plt.imshow(img)
+        plt.axis('off')
+
+
+print(breed_list[2])
+show_dir_images(breed_list[0], 16)
+
+os.mkdir('data')
+for breed in breed_list:
+    os.mkdir('data/' + breed)
+print('Created {} folders to store cropped images of the different breeds.'.format(len(os.listdir('data'))))
+
+for breed in os.listdir('data'):
+    for file in os.listdir('input/stanford-dogs-dataset/annotations/Annotation/{}'.format(breed)):
+        img = Image.open('input/stanford-dogs-dataset/images/Images/{}/{}.jpg'.format(breed, file))
+        tree = ET.parse('input/stanford-dogs-dataset/annotations/Annotation/{}/{}'.format(breed, file))
+        xmin = int(tree.getroot().findall('object')[0].find('bndbox').find('xmin').text)
+        xmax = int(tree.getroot().findall('object')[0].find('bndbox').find('xmax').text)
+        ymin = int(tree.getroot().findall('object')[0].find('bndbox').find('ymin').text)
+        ymax = int(tree.getroot().findall('object')[0].find('bndbox').find('ymax').text)
+        img = img.crop((xmin, ymin, xmax, ymax))
+        img = img.convert('RGB')
+        img = img.resize((224, 224))
+        img.save('data/' + breed + '/' + file + '.jpg')
