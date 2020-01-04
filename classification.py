a0e0a1575453963cce158731282bf659287e28b2
StefanSebastian <stefansebii@gmail.com>
2020-01-04 22:48:43 +0200

    Fixed experiment ; had wrong number of outputs

--- classification.py
+++ classification.py
@@ -1,66 +1,50 @@
+import numpy as np
+import os
+import keras
+import matplotlib.pyplot as plt
+from keras.layers import Dense,GlobalAveragePooling2D
+from keras.applications import MobileNet
+from keras.preprocessing import image
+from keras.applications.mobilenet import preprocess_input
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense 
-from keras import backend as K 
+from keras.models import Model
+from keras.optimizers import Adam
 
-train_data_dir = 'split/train'
-validation_data_dir = 'split/val'
-epochs = 10
-batch_size = 16
-img_width, img_height = 224, 224
-nb_train_samples = 16418
-nb_validation_samples = 4162
+base_model=MobileNet(weights='imagenet',include_top=False) #imports the mobilenet model and discards the last 1000 neuron layer.
 
-if K.image_data_format() == 'channels_first': 
-    input_shape = (3, img_width, img_height) 
-else: 
-    input_shape = (img_width, img_height, 3) 
+x=base_model.output
+x=GlobalAveragePooling2D()(x)
+x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
+x=Dense(1024,activation='relu')(x) #dense layer 2
+x=Dense(512,activation='relu')(x) #dense layer 3
+preds=Dense(120,activation='softmax')(x) #final layer with softmax activation
 
-model = Sequential() 
-model.add(Conv2D(32, (2, 2), input_shape = input_shape)) 
-model.add(Activation('relu')) 
-model.add(MaxPooling2D(pool_size =(2, 2))) 
-  
-model.add(Conv2D(32, (2, 2))) 
-model.add(Activation('relu')) 
-model.add(MaxPooling2D(pool_size =(2, 2))) 
-  
-model.add(Conv2D(64, (2, 2))) 
-model.add(Activation('relu')) 
-model.add(MaxPooling2D(pool_size =(2, 2))) 
-  
-model.add(Flatten()) 
-model.add(Dense(64)) 
-model.add(Activation('relu')) 
-model.add(Dropout(0.5)) 
-model.add(Dense(1)) 
-model.add(Activation('sigmoid')) 
+model=Model(inputs=base_model.input,outputs=preds)
+#specify the inputs
+#specify the outputs
+#now a model has been created based on our architecture
 
-model.compile(loss ='binary_crossentropy', 
-                     optimizer ='rmsprop', 
-                   metrics =['accuracy']) 
-  
-train_datagen = ImageDataGenerator( 
-                rescale = 1. / 255, 
-                 shear_range = 0.2, 
-                  zoom_range = 0.2, 
-            horizontal_flip = True) 
-  
-test_datagen = ImageDataGenerator(rescale = 1. / 255) 
-  
-train_generator = train_datagen.flow_from_directory(train_data_dir, 
-                              target_size =(img_width, img_height), 
-                     batch_size = batch_size, class_mode ='binary') 
-  
-validation_generator = test_datagen.flow_from_directory( 
-                                    validation_data_dir, 
-                   target_size =(img_width, img_height), 
-          batch_size = batch_size, class_mode ='binary') 
-  
-model.fit_generator(train_generator, 
-    steps_per_epoch = nb_train_samples // batch_size, 
-    epochs = epochs, validation_data = validation_generator, 
-    validation_steps = nb_validation_samples // batch_size) 
-  
-model.save_weights('model_saved.h5')
+for i,layer in enumerate(model.layers):
+  print(i,layer.name)
+
+for layer in model.layers:
+    layer.trainable=False
+
+train_datagen=ImageDataGenerator(preprocessing_function=preprocess_input) #included in our dependencies
+
+train_generator=train_datagen.flow_from_directory('./data',
+                                                 target_size=(224,224),
+                                                 color_mode='rgb',
+                                                 batch_size=32,
+                                                 class_mode='categorical',
+                                                 shuffle=True)
+
+model.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'])
+# Adam optimizer
+# loss function will be categorical cross entropy
+# evaluation metric will be accuracy
+
+step_size_train=train_generator.n//train_generator.batch_size
+model.fit_generator(generator=train_generator,
+                   steps_per_epoch=step_size_train,
+                   epochs=10)
--- experiments/1/classification.py
+++ experiments/1/classification.py
@@ -3,6 +3,7 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense 
 from keras import backend as K 
+from keras.optimizers import Adam
 
 train_data_dir = 'split/train'
 validation_data_dir = 'split/val'
@@ -31,14 +32,14 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size =(2, 2))) 
   
 model.add(Flatten()) 
-model.add(Dense(64)) 
+model.add(Dense(512)) 
 model.add(Activation('relu')) 
 model.add(Dropout(0.5)) 
-model.add(Dense(1)) 
+model.add(Dense(120)) 
 model.add(Activation('sigmoid')) 
 
-model.compile(loss ='binary_crossentropy', 
-                     optimizer ='rmsprop', 
+model.compile(loss ='categorical_crossentropy', 
+                     optimizer ='Adam', 
                    metrics =['accuracy']) 
   
 train_datagen = ImageDataGenerator( 
@@ -51,12 +52,12 @@ test_datagen = ImageDataGenerator(rescale = 1. / 255)
   
 train_generator = train_datagen.flow_from_directory(train_data_dir, 
                               target_size =(img_width, img_height), 
-                     batch_size = batch_size, class_mode ='binary') 
+                     batch_size = batch_size, class_mode ='categorical') 
   
 validation_generator = test_datagen.flow_from_directory( 
                                     validation_data_dir, 
                    target_size =(img_width, img_height), 
-          batch_size = batch_size, class_mode ='binary') 
+          batch_size = batch_size, class_mode ='categorical') 
   
 model.fit_generator(train_generator, 
     steps_per_epoch = nb_train_samples // batch_size, 
