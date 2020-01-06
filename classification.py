b253b2be9509a764ca3bc2e3841e926678fecf9d
StefanSebastian <stefansebii@gmail.com>
2020-01-06 21:02:03 +0200

    Demo for classification of random image, model evaluation + confusion matrix

--- classification.py
+++ classification.py
@@ -9,6 +9,14 @@ from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 from keras.optimizers import Adam
+from keras.models import save_model
+
+import pandas as pd  
+import seaborn as sn 
+from sklearn.metrics import confusion_matrix, classification_report
+
+import matplotlib.image as mpimg
+from PIL import Image
 
 base_model = MobileNet(weights='imagenet', include_top=False)
 
@@ -48,7 +56,7 @@ validation_generator = test_datagen.flow_from_directory(
     color_mode='rgb',
     batch_size=32,
     class_mode='categorical',
-    shuffle=True)
+    shuffle=False)
 
 model.compile(optimizer='Adam', loss='categorical_crossentropy',
               metrics=['accuracy'])
@@ -58,8 +66,47 @@ model.compile(optimizer='Adam', loss='categorical_crossentropy',
 
 step_size_train = train_generator.n//train_generator.batch_size
 step_size_validation = validation_generator.n//validation_generator.batch_size
+
+
 model.fit_generator(generator=train_generator,
                     steps_per_epoch=step_size_train,
                     validation_data=validation_generator,
                     validation_steps=step_size_validation,
                     epochs=10)
+
+
+############################################### https://www.kaggle.com/devang/transfer-learning-with-keras-and-mobilenet-v2#Confusion-matrix
+# Classification report + confusion matrix 
+
+validation_generator.reset()
+predictions = model.predict_generator(validation_generator)
+print(predictions)
+classes = validation_generator.classes[validation_generator.index_array]
+y = np.argmax(predictions, axis=-1)
+print(y)
+
+print('Classification Report')
+cr = classification_report(y_true=validation_generator.classes, y_pred=y)
+print(cr)
+
+print('Confusion Matrix')
+plt.clf()
+cm = confusion_matrix(validation_generator.classes, y)
+df = pd.DataFrame(cm, columns=validation_generator.class_indices)
+plt.figure(figsize=(80,80))
+sn.heatmap(df, annot=True)
+plt.savefig('./cm.png')
+
+####################### Classify random img
+imageno=np.random.random_integers(low=0, high=validation_generator.samples)
+
+name = validation_generator.filepaths[imageno]
+print(name)
+plt.imshow(mpimg.imread(name))
+
+img = Image.open(validation_generator.filepaths[imageno]).resize((224, 224))
+probabilities = model.predict(preprocess_input(np.expand_dims(img, axis=0)))
+breed_list = tuple(zip(validation_generator.class_indices.values(), validation_generator.class_indices.keys()))
+
+for i in probabilities[0].argsort()[-3:][::-1]: 
+    print(probabilities[0][i], "  :  " , breed_list[i])
