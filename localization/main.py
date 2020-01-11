9a0b3ec60596e99561a195208f44a934e402544f
viorelieremias <ieremiasviorel@yahoo.com>
2020-01-11 17:56:21 +0200

    - add all localization code to a method

--- localization/main.py
+++ localization/main.py
@@ -2,6 +2,7 @@ import os
 
 import numpy as np
 import torch
+from PIL import ImageDraw
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import transforms
@@ -16,92 +17,112 @@ EPOCH = 10
 BS = 64
 LR = 1e-1
 
-all_img_folder = os.listdir(IMAGES_DIR)
-all_annotation_folder = os.listdir(ANNOTATIONS_DIR)
 
-all_img_name = []
-for img_folder in all_img_folder:
-    img_folder_path = '{}/{}'.format(IMAGES_DIR, img_folder)
-    all_img_name += list(map(lambda x: img_folder + '/' + x, os.listdir(img_folder_path)))
+def main_flow():
+    all_img_folder = os.listdir(IMAGES_DIR)
+    all_annotation_folder = os.listdir(ANNOTATIONS_DIR)
 
-all_annotation_name = []
-for annotation_folder in all_annotation_folder:
-    annotation_folder_path = '{}/{}'.format(ANNOTATIONS_DIR, annotation_folder)
-    all_annotation_name += list(map(lambda x: annotation_folder + '/' + x, os.listdir(annotation_folder_path)))
+    all_img_name = []
+    for img_folder in all_img_folder:
+        img_folder_path = '{}/{}'.format(IMAGES_DIR, img_folder)
+        all_img_name += list(map(lambda x: img_folder + '/' + x, os.listdir(img_folder_path)))
 
-tsfm = transforms.Compose([
-    transforms.Resize([332, 332]),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-])
+    all_annotation_name = []
+    for annotation_folder in all_annotation_folder:
+        annotation_folder_path = '{}/{}'.format(ANNOTATIONS_DIR, annotation_folder)
+        all_annotation_name += list(map(lambda x: annotation_folder + '/' + x, os.listdir(annotation_folder_path)))
 
-test_tsfm = transforms.Compose([
-    transforms.Resize([332, 332]),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-])
+    tsfm = transforms.Compose([
+        transforms.Resize([332, 332]),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ])
 
-train_ds = LocalizationDataset(all_img_name[:int(len(all_img_name) * 0.8)], transform=tsfm)
-train_dl = DataLoader(train_ds, batch_size=BS, shuffle=True)
+    test_tsfm = transforms.Compose([
+        transforms.Resize([332, 332]),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ])
 
-valid_ds = LocalizationDataset(all_img_name[int(len(all_img_name) * 0.8):int(len(all_img_name) * 0.9)], transform=tsfm)
-valid_dl = DataLoader(valid_ds, batch_size=1)
+    train_ds = LocalizationDataset(all_img_name[:int(len(all_img_name) * 0.8)], transform=tsfm)
+    train_dl = DataLoader(train_ds, batch_size=BS, shuffle=True)
 
-test_ds = LocalizationDataset(all_img_name[int(len(all_img_name) * 0.9):], is_test=True, transform=test_tsfm)
-test_dl = DataLoader(test_ds, batch_size=1)
+    valid_ds = LocalizationDataset(all_img_name[int(len(all_img_name) * 0.8):int(len(all_img_name) * 0.9)],
+                                   transform=tsfm)
+    valid_dl = DataLoader(valid_ds, batch_size=1)
 
-train_ds.show_box()
+    test_ds = LocalizationDataset(all_img_name[int(len(all_img_name) * 0.9):], is_test=True, transform=test_tsfm)
+    test_dl = DataLoader(test_ds, batch_size=1)
 
-model = LocalizationModel()
-# model.cuda()
+    train_ds.show_box()
 
-for layer in model.body.parameters():
-    layer.requires_grad = False
+    model = LocalizationModel()
+    # model.cuda()
 
-optm = torch.optim.Adam(model.head.parameters(), lr=LR)
+    for layer in model.body.parameters():
+        layer.requires_grad = False
 
-model.train()
-for epoch in range(1, EPOCH + 1):
-    if epoch == 3:
-        optm = torch.optim.Adam(model.head.parameters(), lr=LR / 2)
-    elif epoch == 5:
-        optm = torch.optim.Adam(model.head.parameters(), lr=LR / 4)
-    elif epoch == 7:
-        optm = torch.optim.Adam(model.head.parameters(), lr=LR / 8)
-    elif epoch == 9:
-        optm = torch.optim.Adam(model.head.parameters(), lr=LR / 10)
+    optm = torch.optim.Adam(model.head.parameters(), lr=LR)
 
-    train_loss = []
-    train_IoU = []
-    for step, data in enumerate(train_dl):
-        imgs, boxes = data
-        # imgs = imgs.cuda()
-        # boxes = boxes.cuda()
+    model.train()
+    for epoch in range(1, EPOCH + 1):
+        if epoch == 3:
+            optm = torch.optim.Adam(model.head.parameters(), lr=LR / 2)
+        elif epoch == 5:
+            optm = torch.optim.Adam(model.head.parameters(), lr=LR / 4)
+        elif epoch == 7:
+            optm = torch.optim.Adam(model.head.parameters(), lr=LR / 8)
+        elif epoch == 9:
+            optm = torch.optim.Adam(model.head.parameters(), lr=LR / 10)
 
-        pred = model(imgs)
-        loss = nn.L1Loss()(pred, boxes.squeeze())
-        train_loss.append(loss.item())
-        IOU = IoU(pred, boxes)
-        train_IoU.append(IOU)
+        train_loss = []
+        train_IoU = []
+        for step, data in enumerate(train_dl):
+            imgs, boxes = data
+            # imgs = imgs.cuda()
+            # boxes = boxes.cuda()
 
-        optm.zero_grad()
-        loss.backward()
-        optm.step()
-        if step % 10 == 0:
-            print('step: ', step, '/', len(train_dl), '\tloss:', loss.item(), '\tIoU:', float(IOU.mean()))
+            pred = model(imgs)
+            loss = nn.L1Loss()(pred, boxes.squeeze())
+            train_loss.append(loss.item())
+            IOU = IoU(pred, boxes)
+            train_IoU.append(IOU)
+
+            optm.zero_grad()
+            loss.backward()
+            optm.step()
+            if step % 10 == 0:
+                print('step: ', step, '/', len(train_dl), '\tloss:', loss.item(), '\tIoU:', float(IOU.mean()))
+
+        model.eval()
+        valid_loss = []
+        valid_IoU = []
+        for step, data in enumerate(tqdm(valid_dl)):
+            imgs, boxes = data
+            # imgs = imgs.cuda()
+            # boxes = boxes.cuda()
+
+            pred = model(imgs)
+            loss = nn.L1Loss()(pred, boxes.squeeze())
+            valid_loss.append(loss.item())
+            IOU = IoU(pred, boxes)
+            valid_IoU.append(IOU.item())
+        print('epoch:', epoch, '/', EPOCH, '\ttrain_loss:', np.mean(train_loss), '\tvalid'
+                                                                                 '_loss:', np.mean(valid_loss),
+              '\tIoU:', np.mean(valid_IoU))
 
     model.eval()
-    valid_loss = []
-    valid_IoU = []
-    for step, data in enumerate(tqdm(valid_dl)):
-        imgs, boxes = data
-        imgs = imgs.cuda()
-        boxes = boxes.cuda()
 
-        pred = model(imgs)
-        loss = nn.L1Loss()(pred, boxes.squeeze())
-        valid_loss.append(loss.item())
-        IOU = IoU(pred, boxes)
-        valid_IoU.append(IOU.item())
-    print('epoch:', epoch, '/', EPOCH, '\ttrain_loss:', np.mean(train_loss), '\tvalid_loss:', np.mean(valid_loss),
-          '\tIoU:', np.mean(valid_IoU))
+    draw_img = []
+    for step, img in enumerate(tqdm(test_dl)):
+        # img = img.cuda()
+        pred = model(img)
+
+        origin_img = transforms.ToPILImage()(img.cpu().squeeze())
+        draw = ImageDraw.Draw(origin_img)
+        xmin, ymin, xmax, ymax = tuple(pred.squeeze().tolist())
+        draw.rectangle(xy=[(int(xmin), int(ymin)), (int(xmax), int(ymax))])
+        draw_img.append(origin_img)
+
+
+main_flow()
